9aea58f428ce20a8e501d965fde851b9807627de
chan <996745885@qq.com>
2020-12-25 23:38:22 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,26 +1,27 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-25 16:46:15
- * @LastEditTime: 2020-12-25 16:52:38
+ * @LastEditTime: 2020-12-25 23:31:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \SimpleTravelApp\src\pages\city\City.vue
 -->
 
 <template>
+  <div>
     <city-header></city-header>
-    
+    <city-search></city-search>
+  </div>
 </template>
 <script>
-import CityHeader from './components/Header'
+import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
 export default {
-    name: 'City',
-    components: {
-        CityHeader
-    }
-    
-}
+  name: "City",
+  components: {
+    CityHeader,
+    CitySearch
+  }
+};
 </script>
-<style lang="stylus" scoped>
-
-</style>
+<style lang="stylus" scoped></style>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,36 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-25 23:25:01
+ * @LastEditTime: 2020-12-25 23:35:20
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \SimpleTravelApp\src\pages\city\components\Search.vue
+-->
+
+<template>
+  <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+<script>
+export default {
+  name: "CitySearch"
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.search
+    padding 0 .1rem
+    height .72rem
+    background $bgColor
+    .search-input
+        box-sizing  border-box
+        height .62rem
+        padding 0 .1rem
+        line-height .62rem
+        width 100%
+        text-align center
+        border-radius .06rem
+        color #666
+
+</style>
